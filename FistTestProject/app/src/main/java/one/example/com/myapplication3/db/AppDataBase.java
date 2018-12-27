2197e982bcab5effcbf11f51ef20674d8b2f5707
Albertrt_Lee <3185017028@qq.com>
2018-12-27 19:52:25 +0800

    FistTestProject

---
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/db/AppDataBase.java
@@ -0,0 +1,11 @@
+package one.example.com.myapplication3.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+import one.example.com.myapplication3.db.dao.UserDao;
+
+@Database(entities = {User.class}, version = 1)
+abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+}
