64ca9108e99dc92e029bd564a2f27e0f9697d80d
Albert_Lee <3185017028@qq.com>
2018-12-28 14:29:46 +0800

    定义1，masterer-Version1.1用于各种第三方框架使用学习使用。

--- FistTestProject/app/src/main/java/one/example/com/myapplication3/Masterer_Version1d1.java
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/Masterer_Version1d1.java
@@ -1,4 +1,9 @@
 package one.example.com.myapplication3;
 
 public interface Masterer_Version1d1 {
+    /**
+     * 1，masterer-Version1.1用于各种第三方框架使用学习使用。
+     * 2，master用于学习Android原生API使用。
+     * 3，masterer-Version1.3用于学习新的语言使用。
+     */
 }
