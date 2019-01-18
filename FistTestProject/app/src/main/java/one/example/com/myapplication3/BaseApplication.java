c4cd287faf095c18d03fec45642e3ca0a35a279f
Albert_Lee <3185017028@qq.com>
2019-01-18 17:08:51 +0800

    Notifacation整理

--- FistTestProject/app/src/main/java/one/example/com/myapplication3/BaseApplication.java
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/BaseApplication.java
@@ -3,7 +3,7 @@ package one.example.com.myapplication3;
 import android.app.Application;
 
 import one.example.com.myapplication3.db.AppDataBase;
-import one.example.com.myapplication3.ui.Notifications.NotificationTools;
+import one.example.com.myapplication3.ui.Notifications.NotificationTools2;
 import one.example.com.myapplication3.utile.ApplicationUtile;
 
 public class BaseApplication extends Application {
@@ -15,16 +15,15 @@ public class BaseApplication extends Application {
         super.onCreate();
         ApplicationUtile.setApplication( this );
         mAppExecutors = new AppExecutors();
-        NotificationTools.registerBoradcastReceiver( this );
+        NotificationTools2.registerBoradcastReceiver( this );
     }
-
 
 
     public AppDataBase getDatabase() {
-        return AppDataBase.getInstance(this, mAppExecutors);
+        return AppDataBase.getInstance( this, mAppExecutors );
     }
 
     public DataRepository getRepository() {
-        return DataRepository.getInstance(getDatabase());
+        return DataRepository.getInstance( getDatabase() );
     }
 }
