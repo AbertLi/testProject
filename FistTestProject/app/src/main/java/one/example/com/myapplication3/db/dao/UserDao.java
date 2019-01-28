bbafa0e2cd08881e67d8dcc3813b6fbe2d9bbb2d
Albert_Lee <3185017028@qq.com>
2019-01-28 16:49:12 +0800

    在dao里面返回Single持有的对象，需要引用一个room:rxjava2的包

--- FistTestProject/app/src/main/java/one/example/com/myapplication3/db/dao/UserDao.java
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/db/dao/UserDao.java
@@ -19,8 +19,9 @@ public interface UserDao {
     @Query("SELECT * FROM user")
     LiveData<List<User>> getAllLiveData();
 
-//    @Query("SELECT * FROM user")//1，猜想当User的数据表已经建好的时候，single和LiveData两种数据持有方式同时存在的时候编译就不能通过了 推翻：单独使用single持有也不行。
-//    Single<List<User>> getAllRxJava();//2，有可能是因为room数据库用的是androidx.room包下面的类，所有不能使用Single持有数据
+    @Query("SELECT * FROM user")//1，猜想当User的数据表已经建好的时候，single和LiveData两种数据持有方式同时存在的时候编译就不能通过了 推翻：单独使用single持有也不行。
+    Single<List<User>> getAllRxJava();//2，有可能是因为room数据库用的是androidx.room包下面的类，所有不能使用Single持有数据
+                                      //3,第二种猜想也是错误的，实际上要在room数据库里面返回Rxjava对象需要引用一个Room：RxJava包。
 
     @Query("SELECT * FROM user WHERE uid IN (:userIds)")
     List<User> loadAllByIds(int[] userIds);
