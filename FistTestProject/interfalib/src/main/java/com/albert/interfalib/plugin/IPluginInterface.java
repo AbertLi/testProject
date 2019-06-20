90a37cb375f1b161a758e05a70819c074c088cad
Albert_Lee <3185017028@qq.com>
2019-06-20 20:54:59 +0800

    接下来测试

--- FistTestProject/interfalib/src/main/java/com/albert/interfalib/plugin/IPluginInterface.java
+++ FistTestProject/interfalib/src/main/java/com/albert/interfalib/plugin/IPluginInterface.java
@@ -1,9 +1,10 @@
 package com.albert.interfalib.plugin;
 
 import android.content.Context;
+import android.view.View;
 
 public interface IPluginInterface {
-    public IPluginViewManager inflate(Context hostContext,Context pluginContext,);
+    public IPluginViewManager inflate(Context hostContext,Context pluginContext);
 
-    public void asseamble();
+    public void asseamble(View rootView,Object o);
 }
