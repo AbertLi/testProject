dc23d4ee4a452853c22dce6f6ae9dda6e4c0f52e
jiAlbert_Lee_company_eclipse <3185017028@qq.com>
2019-11-04 20:31:51 +0800

    1，注释掉data，recyclerview以及相应代码可以编译通过 2，添加脚本可以在win和ninx的不同系统下编译，选择不同路径下的签名文件。 3，脚本里面添加签名和混淆选项。 4，添加不同的res选项和，manifest选项。 5，区分打包成apk和aar包选项。

--- FistTestProject/sample/bundle_one/src/main/java/one/example/com/bundleone/AdViewOneItem.java
+++ FistTestProject/sample/bundle_one/src/main/java/one/example/com/bundleone/AdViewOneItem.java
@@ -1,39 +1,39 @@
-package one.example.com.bundleone;
-
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.Map;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-import one.example.com.myapplication3.R;
-import one.example.com.recyclerview.AbstractView;
-import one.example.com.data.BaseData;
-import one.example.com.recyclerview.BaseViewItem;
-
-/**
- * 定制动态下发的样式 1
- * 大图 H5跳转
- */
-public class AdViewOneItem implements BaseViewItem {
-    private Context mContext;
-    public AdViewOneItem(Context context){
-        mContext = context;
-    }
-
-    @Override
-    public AbstractView inflate(ViewGroup viewGroup,Context context) {
-        View root = LayoutInflater.from(mContext).inflate(R.layout.adview_one_item, viewGroup, false);
-        return new AbstractView(root);
-    }
-
-    @Override
-
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position,
-            BaseData info, Map<String, Object> callback) {
-    }
-}
+//package one.example.com.bundleone;
+//
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import java.util.Map;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//import one.example.com.myapplication3.R;
+//import one.example.com.recyclerview.AbstractView;
+//import one.example.com.data.BaseData;
+//import one.example.com.recyclerview.BaseViewItem;
+//
+///**
+// * 定制动态下发的样式 1
+// * 大图 H5跳转
+// */
+//public class AdViewOneItem implements BaseViewItem {
+//    private Context mContext;
+//    public AdViewOneItem(Context context){
+//        mContext = context;
+//    }
+//
+//    @Override
+//    public AbstractView inflate(ViewGroup viewGroup,Context context) {
+//        View root = LayoutInflater.from(mContext).inflate(R.layout.adview_one_item, viewGroup, false);
+//        return new AbstractView(root);
+//    }
+//
+//    @Override
+//
+//    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position,
+//            BaseData info, Map<String, Object> callback) {
+//    }
+//}
