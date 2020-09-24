6a9215d15b441455142b3d5a3d816348f8f82ff6
jiAlbert_Lee_company_eclipse <3185017028@qq.com>
2020-09-24 15:36:57 +0800

    创建RetrofitAct，问题记录

---
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/ui/retrofit/RetrofitActivity.kt
@@ -0,0 +1,12 @@
+package one.example.com.myapplication3.ui.retrofit
+
+import android.app.Activity
+import android.os.Bundle
+
+class RetrofitActivity:Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
