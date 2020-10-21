475187d839dadec242a240edb2b0a10af30abce9
Albertrt_Lee_BJB <3185017028@qq.com>
2020-10-22 00:38:06 +0800

    Retrofit补充

--- FistTestProject/app/src/main/java/one/example/com/myapplication3/ui/retrofit/eg/NetRequestRetrofitUtil.java
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/ui/retrofit/eg/NetRequestRetrofitUtil.java
@@ -1,5 +1,8 @@
 package one.example.com.myapplication3.ui.retrofit.eg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import one.example.com.myapplication3.ui.retrofit.Content;
@@ -21,16 +24,20 @@ public class NetRequestRetrofitUtil {
                 .build();
 
         GitHubService server = retrofit.create(GitHubService.class);
-        Call call = (Call)server.listRepos("user");
+        Call call = (Call) server.listRepos("user");
         call.enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) {
-                Log.e("TEST","body="+response.body().toString());
+                List<Repo> list = (ArrayList<Repo>) response.body();
+                Log.e("TEST", "body=" + response.body());
+                for (int i = 0; i < list.size(); i++) {
+                    Log.e("TEST", "repo=" + list.get(i).toString());
+                }
             }
 
             @Override
             public void onFailure(Call call, Throwable t) {
-                Log.e("TEST","e = "+t.getMessage());
+                Log.e("TEST", "e = " + t.getMessage());
             }
         });
     }
