221d21af80f5cb26c15a1d10fc7397e8d63557f7
Albertrt_Lee_BJB <3185017028@qq.com>
2021-06-25 23:30:21 +0800

    TextView研究

--- FistTestProject/app/src/main/java/one/example/com/myapplication3/ui/wheelview/WheelViewActivity.kt
+++ FistTestProject/app/src/main/java/one/example/com/myapplication3/ui/wheelview/WheelViewActivity.kt
@@ -1,10 +1,16 @@
 package one.example.com.myapplication3.ui.wheelview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.MotionEvent
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import one.example.com.myapplication3.Logs
 import one.example.com.myapplication3.R
 
-class WheelViewActivity : AppCompatActivity() {
+class WheelViewActivity : Activity() {
+    private var TAG = "WheelViewActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wheel_view)
@@ -14,5 +20,24 @@ class WheelViewActivity : AppCompatActivity() {
             listData.add("$i")
         }
         wheelview.setData(listData)
+        var ll = LinearLayout(this)
+        ll.addView(null)
+//        var rl = RelativeLayout(this)
+//        rl.addView(null)
+
+
+        var systemClockCurrent = SystemClock.currentThreadTimeMillis()
+        var systemClocUptime = SystemClock.uptimeMillis()
+        var systemTime = System.currentTimeMillis()
+        Logs.iprintln(TAG, "systemClockCurrent = $systemClockCurrent   systemClocUptime = $systemClocUptime   systemTime = $systemTime")
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        Logs.iprintln(TAG, "dispatchTouchEvent  action even = ${ev?.action}")
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onUserInteraction() {
+        Logs.iprintln(TAG, "onUserInteraction")
     }
 }
